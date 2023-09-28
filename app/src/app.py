ac0a802cc32cc2e71c58146f4228505f20de4d07
Wizarsy <filipemadruga@outlook.com>
2023-09-28 20:24:36 -0300

    updated blueprints and app config sessions

--- app/src/app.py
+++ app/src/app.py
@@ -15,16 +15,3 @@ app.register_blueprint(auth.bp)
 app.register_blueprint(admin.bp)
 app.register_blueprint(dashboard.bp)
 app.register_blueprint(error.bp)
-
-
-# @app.route("/resultado", methods = ['GET', 'POST'])
-# def resultado():
-#     if request.method == 'POST':
-#         nome = request.form['nome']
-#         email = request.form['email']
-#         senha = request.form['senha']
-#         cur.execute(f"""INSERT INTO Usuario (NOME, EMAIL, SENHA) VALUES
-#         ('{nome}', '{email}', '{senha}')
-#         """)
-#         conn.commit()
-#         return render_template('login.html')
